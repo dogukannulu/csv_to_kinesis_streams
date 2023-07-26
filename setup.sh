64994555771c5935a4d865a4a9ebea415f212548
Dogukan Ulu <dogukannulu@gmail.com>
2023-07-26 22:29:24 +0300

    change all directories

--- setup.sh
+++ setup.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-cd /data
-
 # install pip3 and python3
 sudo yum update
 sudo yum install python3
 sudo yum install python3-pip
 
 # download the zip file
+cd /data
 sudo curl -O https://github.com/dogukannulu/csv_to_kinesis_streams/blob/main/csv_to_kinesis.zip
 
 # Unzip the files
+cd /data
 sudo unzip csv_to_kinesis.zip
 
 # Navigate to the extracted directory
-cd csv_to_kinesis
+cd /data/csv_to_kinesis
 
 # Install the required libraries
+cd /data/csv_to_kinesis
 pip3 install -r requirements.txt
 
 # Execute the Python script
+cd /data/csv_to_kinesis
 sudo chmod +x csv_to_kinesis.py
 python3 data_streams.py --stream_name streaming-ec2-csv --interval 2 --max_rows 60 --skip-header
